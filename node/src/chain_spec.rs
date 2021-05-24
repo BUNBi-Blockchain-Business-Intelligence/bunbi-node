d60b7657462c778b74023f3a2c78bc995f941e1c
Jacobo Rodrigo Hernandez Mendieta <jacobohernandezmendieta7@gmail.com>
2021-05-24 14:11:39 -0500

    red id

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -122,8 +122,8 @@ pub fn subsocial_staging_config() -> Result<ChainSpec, String> {
     let wasm_binary = WASM_BINARY.ok_or("Staging wasm binary not available".to_string())?;
 
     Ok(ChainSpec::from_genesis(
-        "Subsocial",
-        "subsocial",
+        "Bunbinetwork",
+        "bunbinetwork",
         ChainType::Live,
         move || testnet_genesis(
             wasm_binary,
